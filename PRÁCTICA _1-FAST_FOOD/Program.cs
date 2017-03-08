04ba29a09f297c16fe3afa61e671720283fe4602
sergio <sergio@MSI>
2017-03-08 20:55:07 +0100

    Add project files.

---
+++ PRÁCTICA _1-FAST_FOOD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRÁCTICA__1_FAST_FOOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
